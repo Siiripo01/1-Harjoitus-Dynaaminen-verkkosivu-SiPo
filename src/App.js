81a86d80172bd6963fd7e15895d309587b43442f
Siiri Poropudas <n1posi00@students.oamk.fi>
2022-02-04 19:38:30 +0200

    Tehtävän rakennusta & CSS muokkaus

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-
+import {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [bottles, setBottles] = useState(0);
+  const [time, setTime] = useState(0);
+  const [gender, setGender] = useState('male');
+  const [result, setResult] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    let alcoholLevel = 0;
+    let litres = bottles * 0.33;
+    let grams = litres * 8 * 4.5;
+    let burning = weight / 10;
+    let gramsLeft = grams - burning * time;
+
+    if (gender === 'male') {
+      alcoholLevel = gramsLeft / (weight * 0.7);
+    }
+    else {
+      alcoholLevel = gramsLeft / (weight * 0.6);
+    }
+    setResult(alcoholLevel);
+
+    if(alcoholLevel < 0){
+      alcoholLevel = 0
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h3>Alcohol level</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Weight</label>
+          <input name="weight" type="number" step="1" value={weight} onChange={e => setWeight(e.target.value)}></input>
+        </div>
+        <div>
+          <label>Bottles</label>
+          <select name="bottles" value={bottles} onChange={e => setBottles(e.target.value)}>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </div>
+        <div>
+          <label>Time</label>
+          <select name="time" value={time} onChange={e => setTime(e.target.value)}>
+            <option value="1">1h</option>
+            <option value="2">2h</option>
+            <option value="3">3h</option>
+            <option value="4">4h</option>
+            <option value="5">5h</option>
+          </select>
+        </div>
+        <div>
+          <label>Gender</label>
+          <input type="radio" name="gender" value="male" defaultChecked onChange={e => setGender(e.target.value)}/><label>Male</label>
+          <input type="radio" name="gender" value="female" onChange={e => setGender(e.target.value)}/><label>Female</label>
+        </div>
+        <div>
+          <output>{result.toFixed(2)}</output>
+        </div>
+        <button>Calculate</button>
+      </form>
+    </>
   );
 }
 
